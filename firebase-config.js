a4abdc94f6b17ed204d1ac911613f3f602b99701
rezo97 <revazzurabishvili@gmail.com>
2025-12-04 16:30:30 +0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,18 +1,19 @@
-// firebase-config.js
+// firebase-config.js (თქვენი მონაცემებით)
 
-// 1. Firebase-ის კონფიგურაცია (შეცვალეთ თქვენი მონაცემებით)
+// 1. Firebase-ის კონფიგურაცია (თქვენი პირადი მონაცემები)
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY", 
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyB6zReg3Z_f3jEm6gnQG76D_azm63zXL2E", 
+    authDomain: "warehouse-scanner-2cd7d.firebaseapp.com",
+    projectId: "warehouse-scanner-2cd7d",
+    storageBucket: "warehouse-scanner-2cd7d.firebasestorage.app",
+    messagingSenderId: "231541585722",
+    appId: "1:231541585722:web:de72b26115224154c40203",
+    // measurementId: "G-P0HNC85XLH" // არ არის საჭირო Firestore-ისთვის
 };
 
 // 2. Firebase-ის ინიციალიზაცია
 firebase.initializeApp(firebaseConfig);
 
 // 3. Firestore-ის ინიციალიზაცია და გლობალურ ცვლადში შენახვა
-// *** ეს ხაზი ქმნის ცვლადს "db" ***
+// *** ეს ქმნის გლობალურ ცვლადს "db" ***
 const db = firebase.firestore();
